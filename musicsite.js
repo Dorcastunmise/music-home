0576c1b832623c0ddf0178a02b91fee273d7077b
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-07-13 20:37:08 +0100

    access

--- musicsite.js
+++
@@ -1,9 +0,0 @@
-setInterval(myFunction, 2000);
-
-            function myFunction() {
-            let d = new Date();
-
-                document.getElementById('demo').innerHTML = d.getHours() + ":" +
-                d.getMinutes() + ":" + 
-                d.getSeconds();
-            }
